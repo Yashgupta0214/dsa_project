b90389ac2648fb5c90cc188c0f86e2e30f52a99a
Yashgupta0214 <yashgupta2122007@gmail.com>
2026-09-19 12:57:18 +0530

    Allow public auth and invite routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"]
+  publicRoutes: ["/api/uploadthing", "/sign-in(.*)", "/sign-up(.*)", "/invite(.*)"]
 });
 
 export const config = {
